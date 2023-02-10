fc89acbaa0f10a254bf5790c0f007890fb82afa0
IRAVARAJU SASI KIRANMAI <120200129+SASIKIRANMAI@users.noreply.github.com>
2023-02-10 10:41:56 +0530

    Add files via upload

---
+++ 2(9)jpg to png.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+im1 = Image.open(r"C:\Users\SUCHARITHA\Dropbox\My PC (LAPTOP-OUTRAMR1)\Desktop\folders\Cherry\IMG-20200607-WA0003.jpeg")
+im1.save(r"C:\Users\SUCHARITHA\Dropbox\My PC (LAPTOP-OUTRAMR1)\Desktop\folders\Cherry\IMG-20200607-WA0003.png")
